48a2bcc948687c7176eea985d0b653c9be18571e
Oliver Horwood <thereviloproject@gmail.com>
2021-06-06 18:24:34 +0100

    Added player selection and computer selection as global variables and started function for game to play 1 round

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function computerPlay(){
-    var computerChoices = ["Rock", "Paper", "Scissors"];
-    var choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+function computerPlay() {
+    let computerChoices = ["Rock", "Paper", "Scissors"];
+    let choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(choice);
 }
 
 computerPlay();
 
+const playerSelection = prompt("Please enter your choice: ");
+const computerSelection = computerPlay();
 
+function singleRound(playerSelection, computerSelection) {
+    
+}
 
 
     
