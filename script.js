ab88010c6b82e7d9b39eb5b1b480fafa711a10ff
Oliver Horwood <thereviloproject@gmail.com>
2021-06-06 17:46:33 +0100

    Created function that tells the computer to choose their play automatically

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-<script>
+function computerPlay(){
+    var computerChoices = ["Rock", "Paper", "Scissors"];
+    var choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(choice);
+}
 
-
+computerPlay();
 
 
 
 
     
-</script>
