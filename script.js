7b458610a6dabd29a2fed2d91a0fa47132825207
Oliver Horwood <thereviloproject@gmail.com>
2021-06-13 23:32:13 +0100

    added var for player input athat also converts input to lower case and a var to declare the computerPlay function

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ function computerPlay() {
 // Logs the result of the function.... it works 
 console.log(computerPlay()); 
 
+// Allows user input through the prompt command and converts anything typed into the box to lower case
+let playerSelection = prompt("please enter rock, paper, or scissors!").toLowerCase();
+// computerSelection is the result of the computerPlay function
+const computerSelection = computerPlay();
+// Logged the playerSelection variable to see whether the toLowerCase worked... it does
+console.log(playerSelection);
 
+function singleRound(playerSelection, computerSelection) {
+
+}
 
 
 
