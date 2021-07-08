7de70ed03afb6e6f1eb17d3c449a1434581765b9
Oliver Horwood <thereviloproject@gmail.com>
2021-07-08 09:40:59 +0100

    added a comment for event listener function

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
 
-console.log(rockButton);
+// Function to randomly pick out of the available options
+function computerPlay() {
+    // Array of the possible options the computer is allowed to choose from
+    const computerChoices = ["rock", "paper", "scissors"];
+    // Chooses a random string out of the array above
+    const choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Calls the choice variable
+    return choice;
+} 
 
-
+// When the rock button is clicked then execute the function with the curly brackets
+rockButton.addEventListener('click', function() {
+    console.log("You chose rock");
+})
 
 
 
