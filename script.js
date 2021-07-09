5ab13c39fe191808c2cc21425a64bc1796848fe6
Oliver Horwood <thereviloproject@gmail.com>
2021-07-09 10:44:01 +0100

    added player/computer score variables to keep track of the rounds and when to end the game

--- script.js
+++ script.js
@@ -7,36 +7,51 @@ function computerPlay() {
     const choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     // Calls the choice variable
     return choice;
-}
+} 
 
-const allButtons = document.querySelectorAll(".buttons");
+    
 
-allButtons.forEach(e => {e.addEventListener('click', function() { 
-    const computerSelection = computerPlay();
-    console.log(computerSelection);
+    const allButtons = document.querySelectorAll(".buttons");
+    allButtons.forEach(e => {e.addEventListener('click', function() {
+        const computerSelection = computerPlay();
+        console.log(computerSelection);
     if (computerSelection === "rock" && e.innerHTML === "Rock") {
         document.getElementById("result").innerHTML = "It's a tie this round!";
     } else if (computerSelection === "rock" && e.innerHTML === "Paper") {
+        ++playerScore;
         document.getElementById("result").innerHTML = "You win this round!";
     } else if (computerSelection === "rock" && e.innerHTML === "Scissors") {
+        ++computerScore;
         document.getElementById("result").innerHTML = "You lose this round!";
     }
     if (computerSelection === "paper" && e.innerHTML === "Paper") {
         document.getElementById("result").innerHTML = "It's a tie this round!";
     } else if (computerSelection === "paper" && e.innerHTML === "Rock") {
+        ++computerScore;
         document.getElementById("result").innerHTML = "You lose this round!";
     } else if (computerSelection === "paper" && e.innerHTML === "Scissors") {
+        ++playerScore;
         document.getElementById("result").innerHTML = "You win this round!";
     }  
     if (computerSelection === "scissors" && e.innerHTML === "Scissors") {
         document.getElementById("result").innerHTML = "It's a tie this round!";
     } else if (computerSelection === "scissors" && e.innerHTML === "Rock") {
+        ++playerScore;
         document.getElementById("result").innerHTML = "You win this round!";
     } else if (computerSelection === "scissors" && e.innerHTML === "Paper") {
+        ++computerScore;
         document.getElementById("result").innerHTML = "You lose this round!";
     }
+    console.log(playerScore, computerScore);
 })
 })
+
+      let playerScore = 0;
+      let computerScore = 0;  
+      
+
+
+
 
 
 
