fed885746155252e56fb57569c0bf258917d5d88
Oliver Horwood <thereviloproject@gmail.com>
2021-07-09 02:32:21 +0100

    added a working queryselectorall for the buttons and made a function that loops through each buttons and if they are clicked to console.log the innerHTML content. This is for testing before i add useful function data to the foreach loop

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const rockButton = document.querySelector(".rock");
-const paperButton = document.querySelector(".paper");
-const scissorsButton = document.querySelector(".scissors");
 
-// Function to randomly pick out of the available options
-function computerPlay() {
-    // Array of the possible options the computer is allowed to choose from
-    const computerChoices = ["rock", "paper", "scissors"];
-    // Chooses a random string out of the array above
-    const choice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    // Calls the choice variable
-    return choice;
-} 
+const allButtons = document.querySelectorAll(".buttons");
+console.log(allButtons);
 
-// When the rock button is clicked then execute the function with the curly brackets
-rockButton.addEventListener('click', function() {
-    console.log("You chose rock");
+allButtons.forEach(e => {e.addEventListener('click', function() { 
+    console.log(e.innerHTML);
 })
+})
+
+
+
 
 
 
